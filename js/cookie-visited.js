2ef4ad1cb8a15729cf002a1a058afc1ec427f970
Your Name <you@example.com>
2020-04-13 13:20:23 +0000

    fix

--- js/cookie-visited.js
+++
@@ -1,6 +0,0 @@
-if(document.cookie.indexOf('visited-' + location.hostname) == -1)
-{
-    var day = 7*86400000;
-    document.cookie = 'visited=visited-' + location.hostname + ';max-age=' + day + ';domain=' + location.hostname + ';path=/';
-    $('#myModal').css('display','block');
-}
